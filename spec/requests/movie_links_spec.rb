a12a1731cf26afd0abe9dec4e8c1caf16022acaa
Anderson Mesquita <andersonvom@gmail.com>
2012-03-11 22:10:04 -0400

    Added tests for storing user preferences (sort and filter) in session

--- spec/requests/movie_links_spec.rb
+++ spec/requests/movie_links_spec.rb
@@ -18,4 +18,13 @@ describe "Movie Links" do
     assigns(:movies).where_values_hash.should == {:rating=>["G"]}
   end
 
+  it "should remember the sort and filter settings" do
+    visit movies_path
+    check 'ratings_G'
+    click_button "Refresh"
+    click_link "Movie Title"
+    get movies_path
+    response.should redirect_to movies_path(:order_by=>'title', :ratings=>{"G"=>1})
+  end
+
 end
